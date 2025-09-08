42081aaba480910acf736c37a28cad4986c220d1
정관영 <kwangdss@users.noreply.github.com>
2025-09-08 21:37:12 +0900

    Initial commit: BuildUp Gradle project

---
+++ src/main/java/com/smhrd/buildUp/BuildUpApplication.java
@@ -0,0 +1,13 @@
+package com.smhrd.buildUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smhrd/buildUp/BuildUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smhrd.buildUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
